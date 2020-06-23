78a4028c361b15491f421e84ad9512b193aab9c7
Manish Goregaokar <manishsmail@gmail.com>
2020-06-23 14:33:47 -0700

    Add Display impl for Script

--- src/lib.rs
+++ src/lib.rs
@@ -112,6 +112,12 @@ impl From<char> for Script {
     }
 }
 
+impl fmt::Display for Script {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.full_name())
+    }
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 #[non_exhaustive]
 /// A value for the `Script_Extension` property
